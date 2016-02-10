de4d099a9884cd8c8ee59245c2cebc5888482b5d
Kieran Eglin <kieran.eglin@gmail.com>
2016-02-10 15:21:21 -0800

    Included express

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var server = http.createServer(function(request, response){
-  response.writeHead(200, {"Content-Type" : "text/plain"});
-  response.write("hello");
-  response.end();
-});
