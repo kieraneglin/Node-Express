1d4252429aa39ded04036c2eff2986ead14b7e2f
Kieran Eglin <kieran.eglin@gmail.com>
2016-02-14 14:09:45 -0800

    Added jade to handle views

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+
 app.get('/', function(req, res){
-  res.send('Hello Express');
+  res.render('default', {title: 'Home'});
+});
+app.get('/me', function(req, res){
+  res.send('Its me!');
+});
+app.get('/who/:name?', function(req, res){
+  var name = req.params.name;
+  res.send(name + ' wuz here');
+});
+app.get('/who/:name?/:title?', function(req, res){
+  var name = req.params.name;
+  var title = req.params.title;
+  res.send(name + ' wuz here' + title);
+});
+app.get('*', function(req, res){
+  res.send('Bad route');
 });
 
 var server = app.listen(3000, function(){
